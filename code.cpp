1762f00abfb249e71b31073369b9341dde3f577c
lovelysingh033 <lovelysinghknp583@gmail.com>
2020-11-05 20:59:51 +0530

    first commit

---
+++ code.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main() {
+   char ch;
+   cin>>ch;
+   int a=ch;
+   //First method
+   if(a>=65 && a<=90)
+   cout<<"uppercase"<<endl;
+   else if(a>=97 && a<=122)
+   cout<<"lowercase"<<endl;
+   else
+   cout<<"invalid"<<endl;
+   //second method
+   if(ch>='A' && ch<='Z')
+   cout<<"Uppercase"<<endl;
+   else if
+   (ch>='a' && ch<='z')
+   cout<<"Lowercase"<<endl;
+   else
+   cout<<"Invalid";
+   return 0;
+}
